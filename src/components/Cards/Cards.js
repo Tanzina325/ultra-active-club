b0986e3e9f3bcf1f24b76736b4cdd1ddef5f4592
01792166684 <tanzinatania491991@gmail.com>
2022-09-29 07:46:02 +0600

    seventh commit

--- src/components/Cards/Cards.js
+++ src/components/Cards/Cards.js
@@ -1,12 +1,13 @@
 import React, { useEffect, useState } from 'react';
-import BreakTime from '../BreakTime/BreakTime';
+// import BreakTime from '../BreakTime/BreakTime';
 import Card from '../Card/Card';
 import ExerciseTime from '../ExerciseTime/ExerciseTime';
 import './Cards.css'
 const Cards = () => {
     const [cards, setCards ]=useState([]);
     const [exerciseTime,setExerciseTime] = useState([]);
-
+    const [breakTime,setBreakTime] = useState(0);
+    
     useEffect(()=>{
       fetch('Fakedata.json')
       .then(res=>res.json())
@@ -17,6 +18,7 @@ const Cards = () => {
         const newExerciseTime = [...exerciseTime, card];
         setExerciseTime(newExerciseTime)
     }
+const breakTimeAdded =()=>{setBreakTime(breakTime+15)}
 
     return (
         <div className="card-container">
@@ -35,39 +37,39 @@ const Cards = () => {
           </div>
           <div className="sidebar-menu">
             <div className='profile'>
-            <div className='trainer-image'><img src="https://thumbs.dreamstime.com/z/serious-looking-old-man-portrait-29761931.jpg" alt="" /></div>
+            <div className='trainer-image'><img src="https://qph.fs.quoracdn.net/main-qimg-877218280ebe36422d8b5e95913fedd0" alt="" /></div>
             <div className='Introductory'>
-                <h4>Matiar Rahaman</h4>
-                <p>Tokio,Japan</p>
+                <h4>Tanzina Rahaman</h4>
+                <p>Potukhali,Barisal</p>
             </div>
             </div>
             <div className='details'>
             <div>
-            <h3>75kg</h3>
+            <h3>56kg</h3>
             <p>Weight</p>
             </div>
             <div>
-            <h3> 5.8</h3>
+            <h3> 5.2</h3>
             <p>Height</p>
             </div>
             <div>
-            <h3>70yrs </h3>
+            <h3>30yrs </h3>
             <p>Age</p>
             </div>
             </div>
             <h2>Add a break</h2>
             <div className='details'>
-            <button>10s</button>
-            <button>20s</button>
-            <button>30s</button>
-            <button>40s</button>
+            <button onClick={breakTimeAdded}>10s</button>
+            <button onClick={breakTimeAdded}>20s</button>
+            <button onClick={breakTimeAdded}>30s</button>
+            <button onClick={breakTimeAdded}>40s</button>
             </div>
             <h2>Exercise Details</h2>
             <div className='exercise-time'>
             <ExerciseTime exerciseTime={exerciseTime} ></ExerciseTime>
             </div>
             <div className='exercise-time'>
-             <BreakTime></BreakTime>
+             <h3>Break Time : <span className='break-time'>{breakTime} seconds</span></h3>
             </div>
             <button className='btn-activity'>Activity Completed</button>
             </div>  
