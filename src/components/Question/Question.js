385450e8ac283c5d078f222dfe2b3970fca6cd8c
01792166684 <tanzinatania491991@gmail.com>
2022-12-22 15:58:34 +0600

    question added

--- src/components/Question/Question.js
+++ src/components/Question/Question.js
@@ -12,7 +12,7 @@ const Question = () => {
                 <p>Props are used to pass data, whereas state is for managing data Data from props is read-only, and cannot be modified by a component that is receiving it from outside State data can be modified by its own component, but is private (cannot be accessed from outside) Props can only be passed from parent component to child (unidirectional flow)</p>
             </div>
             <div className='question'>
-                <h3>What are the uses of useEffect without fething ?</h3>
+                <h3>What are the uses of useEffect without fething </h3>
                 <p>Fetching data from an API, communicating with a database, and sending logs to a logging service are all considered side-effects, as it's possible to have a different output for the same input. For example, your request might fail, your database might be unreachable, or your logging service might have reached its quota.This is why useEffect is the hook for us - by fetching data, we're making our React component impure, and useEffect provides us a safe place to write impure code.</p>
             </div>
         </div>
